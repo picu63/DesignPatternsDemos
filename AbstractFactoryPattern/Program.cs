2de15547070ee40b443f9174ee22fed80be08fa1
picu63 <picu63@gmail.com>
2022-04-11 00:53:01 +0200

    fix naming

--- AbstractFactoryPattern/Program.cs
+++ AbstractFactoryPattern/Program.cs
@@ -6,11 +6,11 @@ var invoice = new Invoice(1, "Walmart", "Monthly subscription", 19.99);
 Console.WriteLine($"Created invoice: {JsonSerializer.Serialize(invoice)}");
 IInvoiceFactory<PdfDocument> pdfFactory = new PdfInvoiceFactory("pdfs\\.");
 var pdfFile = pdfFactory.CreateDocument(invoice);
-PrintDocument(pdfFile);
+PrintDocumentInfo(pdfFile);
 IInvoiceFactory<XmlDocument> xmlFactory = new XmlInvoiceFactory("xmls\\.");
 var xmlFile = xmlFactory.CreateDocument(invoice);
-PrintDocument(xmlFile);
-static void PrintDocument(IDocument document)
+PrintDocumentInfo(xmlFile);
+static void PrintDocumentInfo(IDocument document)
 {
     Console.WriteLine($"Document name: {document.Name}, data: {System.Text.Encoding.UTF8.GetString(document.Data)}");
 }
