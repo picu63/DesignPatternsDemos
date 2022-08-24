5feef83f440797a3175e1a974ca9f3b676c759f9
Piotr Olearczyk <piotr.olearczyk@medius.com>
2022-08-24 13:48:39 +0200

    added singleton pattern

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+var instance1 = Logger.Instance;
+var instance2 = Logger.Instance;
+
+if (instance1 == instance2)
+{
+    Console.WriteLine("Instances are the same");
+}
+
+
+public class Logger
+{
+    private static readonly Lazy<Logger> LazyLogger = new(() => new Logger());
+    public static Logger Instance => LazyLogger.Value;
+}
